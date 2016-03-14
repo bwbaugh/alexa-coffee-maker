fea5bb5a9252e009866e0a3d566966b039ba7c3b
Wesley Baugh <wesley@bwbaugh.com>
2016-03-13 17:15:52 -0700

    Create helper function for a bad response

--- index.py
+++ index.py
@@ -72,34 +72,37 @@ def get_welcome_response():
 
 def get_scoops_for_cups(intent, session):
     """Calculate the number of scoops to use for some cups of water."""
-    if 'Cups' in intent['slots']:
-        try:
-            num_cups = int(intent['slots']['Cups']['value'])
-        except KeyError:
-            speech_output = "I'm not sure how many cups you're making."
-            should_end_session = False
-        else:
-            speech_output = (
-                'Use {num_scoops:.2g} scoops for {num_cups} cups.'
-            ).format(
-                num_scoops=num_cups * SCOOPS_PER_CUP,
-                num_cups=num_cups,
-            )
-            should_end_session = True
-    else:
-        speech_output = "I'm not sure how many cups you're making."
-        should_end_session = False
+    def bad_response():
+        return build_response(
+            session_attributes={},
+            speechlet_response=build_speechlet_response(
+                card_title=None,
+                output="I'm not sure how many cups you're making.",
+                reprompt_text="How many cups of coffee are you making?",
+                should_end_session=False,
+            ),
+        )
+
+    try:
+        num_cups = int(intent['slots']['Cups']['value'])
+    except KeyError:
+        return bad_response()
     return build_response(
         session_attributes={},
         speechlet_response=build_speechlet_response(
             card_title='Coffee scoops',
-            output=speech_output,
+            output=(
+                'Use {num_scoops:.2g} scoops for {num_cups} cups.'
+            ).format(
+                num_scoops=num_cups * SCOOPS_PER_CUP,
+                num_cups=num_cups,
+            ),
             # Setting reprompt_text to None signifies that we do not
             #   want to reprompt the user. If the user does not respond
             #   or says something that is not understood, the session
             #   will end.
             reprompt_text=None,
-            should_end_session=should_end_session,
+            should_end_session=True,
         ),
     )
 
